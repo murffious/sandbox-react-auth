892d397bfad774135e6b34ba4e6a802731c9771e
Paul Murff <titan.murff@gmail.com>
2017-08-31 15:46:40 -0600

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'})
+    res.send({hey: 'jones'})
 })
 
 const PORT = process.env.PORT || 5000
