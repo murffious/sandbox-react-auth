cc1e01dbaa7586194a6ae05d293b7117d644221e
Paul Murff <titan.murff@gmail.com>
2017-08-31 17:38:14 -0600

    first one for server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hey: 'jones'})
-})
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
