0961ead646727a828c897625a56da3b6cdd87765
Paul Murff <titan.murff@gmail.com>
2017-08-31 21:01:41 -0600

    done auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// production keys here
+module.exports = {
+    
+     googleClientID: process.env.GOOGLE_CLIENT_ID,
+     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+     mongoURI: process.env.MONGO_URI,
+     cookieKey: process.env.COOKIE_KEY
+     
+         
+     };
+     
