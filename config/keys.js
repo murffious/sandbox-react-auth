e01266468f4790e21b7a574422db4989ad6b30df
Paul Murff <titan.murff@gmail.com>
2017-08-31 22:54:25 -0600

    basic skeleton with auth flow

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
-   module.exports = require('.dev');
+   module.exports = require('./dev');
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
+const keys = require('./config/dev');
 require('./models/User');
 require('./services/passport');
 
